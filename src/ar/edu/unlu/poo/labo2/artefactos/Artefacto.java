6c8fce14dfd1a4a38df413cdf18b1d84fd0447a0
candelamaniscalco <127630567+candelamaniscalco@users.noreply.github.com>
2025-10-14 20:38:42 -0300

    prueba

--- src/ar/edu/unlu/poo/labo2/artefactos/Artefacto.java
+++ src/ar/edu/unlu/poo/labo2/artefactos/Artefacto.java
@@ -1,4 +1,47 @@
 package ar.edu.unlu.poo.labo2.artefactos;
+import java.util.Objects;
 
-public class Artefacto {
+public  class Artefacto {
+    private String nombre;
+    private int poder; //VALIDAR QUE ESTE ENTRE 1 Y 100
+    private String tipo; //POCION-PERGAMINO-AMULETO-VARITA
+
+    public Artefacto(String nombre, int poder, String tipo) {
+        this.nombre = nombre;
+        this.poder = poder;
+        this.tipo = tipo;
+    }
+
+    public void setPoder(int poder) {
+        this.poder = poder;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getPoder() {
+        return poder;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Artefacto artefacto = (Artefacto) o;
+        return Objects.equals(nombre, artefacto.nombre);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nombre);
+    }
+
+    public Object getNivelDePoder() {
+        return null;
+    }
 }
--- src/ar/edu/unlu/poo/labo2/artefactos/CatalogoArtefactos.java
+++ src/ar/edu/unlu/poo/labo2/artefactos/CatalogoArtefactos.java
@@ -1,4 +1,78 @@
 package ar.edu.unlu.poo.labo2.artefactos;
 
+import ar.edu.unlu.poo.labo2.artefactos.Artefacto;
+import ar.edu.unlu.poo.labo2.artefactos.ValidadorDeArtefactos;
+
+import java.util.*;
+
 public class CatalogoArtefactos {
+    //COLECCION QUE EVITA DUPLICADOS
+    Set<Artefacto> inventario = new HashSet<>();
+    private ValidadorDeArtefactos miValidador;
+
+    //METODOS
+
+    public CatalogoArtefactos(ValidadorDeArtefactos validador){
+        this.miValidador = validador;
+    }
+
+    public void agregarArtefacto(Artefacto artefacto) {
+        inventario.add(artefacto);
+        //System.out.printf("Artefacto: %s agregado al inventario con éxito.\n", artefacto.getNombre().toUpperCase(Locale.ROOT));
+    }
+
+    public Set obtenerArtefactosUnicos() {
+        return inventario;
+    }
+
+    public List<Artefacto> buscarArtefactosPorTipo(String tipo) {
+        //Devuelve una lista de artefactos que coincidan con el tipo especificado.
+        //La lista debe estar ordenada de mayor a menor poder.
+        List<Artefacto> artefactosCoincidentes = new ArrayList<>();
+        for (Artefacto artefacto : inventario) {
+            String tipoBuscado = artefacto.getTipo();
+            if (tipoBuscado.equals(tipo)) {
+                artefactosCoincidentes.add(artefacto);
+            }
+        }
+
+        Collections.sort(artefactosCoincidentes, new Comparator<Artefacto>() {
+            @Override
+            public int compare(Artefacto a1, Artefacto a2) {
+                return Integer.compare(a2.getPoder(), a1.getPoder());
+                //escribo primero el segundo elemento pq de esta manera ordena del mayor a menor, si fuera al reves ordena de menor a mayor
+            }
+        });
+        return artefactosCoincidentes;
+    }
+
+    public Map<String, Integer> contarArtefactosPorTipo() {
+        Map<String, Integer> cantArtefactosPorTipo = new HashMap<>();
+
+        for (Artefacto artefacto : inventario) {
+            String tipo = artefacto.getTipo();
+            //int conteoActual = cantArtefactosPorTipo.get(tipo); //en la primer vuelta almacena null
+            //cantArtefactosPorTipo.put(tipo, conteoActual + 1);
+            cantArtefactosPorTipo.put(tipo, (cantArtefactosPorTipo.getOrDefault(tipo, 0)) + 1);
+            //getOrDefault(tipo, defaultValue) va a devolver el valor actual si es que existe o 0 si todavia no estaba en el Map.
+        }
+        return cantArtefactosPorTipo;
+    }
+
+    public Artefacto obtenerArtefactoMasPoderoso() {
+        if (inventario.isEmpty()) {
+            return null;
+        }
+        Artefacto mayorAux = null;
+        for (Artefacto artefacto : inventario) {
+            if (mayorAux == null || (artefacto.getPoder() > mayorAux.getPoder())) {
+                mayorAux = artefacto;
+            }
+        }
+        return mayorAux;
+    }
+
+    public void aplicarMejoras() {
+
+    }
 }
